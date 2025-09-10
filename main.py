18bc3b91c8dd69aba9d7996bdf472b0da864d482
Clément ULRICH <clement.ulrich79@gmail.com>
2025-09-10 19:55:16 +0200

    Suite exercice jusqu'au 3.3

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ data = pd.read_csv("vgsales.csv")
 
 
 ## Exercice 2
+print(" ")
+print("-------------- Exercice 2 --------------")
+
 print("Shape of dataset:", data.shape)
 print("Missing values per column:\n", data.isnull().sum())
 print("Number of duplicate rows:", data.duplicated().sum())
@@ -22,6 +25,8 @@ print(data_clean.info())
 
 
 ## Exercice 3.1
+print(" ")
+print("-------------- Exercice 3.1 --------------")
 print("\nStatistiques descriptives du jeu de données :")
 print(data_clean.describe())
 
@@ -33,4 +38,52 @@ print(corr_matrix)
 
 ## Exercice 3.2
 # Afficher les colonnes existantes
-print(data_clean.columns)
+global_sales_by_year = data_clean.groupby('Year')['Global_Sales'].sum()
+print(" ")
+print("-------------- Exercice 3.2 --------------")
+print(global_sales_by_year)
+
+# Tracer l'évolution des ventes globales par année
+global_sales_by_year.plot(kind='line', marker='x', figsize=(10,6))
+plt.title('Évolution des ventes  par année')
+plt.xlabel('Année'), plt.ylabel('Ventes mondiales (millions)')
+plt.grid(True)
+plt.show()
+
+# Année avec le plus de ventes au total
+most_selling_year = global_sales_by_year.idxmax()
+most_selling_value = global_sales_by_year.max()
+print(" ")
+print(f"L'année avec le plus de ventes mondiales est : {most_selling_year} ({most_selling_value} millions)")
+
+
+## Exercice 3.3
+print(" ")
+print("-------------- Exercice 3.3 --------------")
+
+publisher_sales = data_clean.groupby('Publisher')['Global_Sales'].sum()
+total_global_sales = data_clean['Global_Sales'].sum()
+publisher_sales_percentage = publisher_sales / total_global_sales * 100
+publisher_sales_percentage = publisher_sales_percentage.sort_values(ascending=False)
+print(publisher_sales_percentage)
+
+# Tracer le graphique en barres
+plt.figure(figsize=(12, 8))
+publisher_sales_percentage.plot(kind='bar', color='skyblue')
+plt.title('Pourcentage des ventes mondiales par éditeur')
+plt.xlabel('Éditeur')
+plt.ylabel('Pourcentage (%)')
+plt.xticks(rotation=45, ha='right')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+
+# Sélectionner les 10 éditeurs avec les ventes les plus élevées
+top_publishers = publisher_sales.sort_values(ascending=False).head(10)
+print(" ")
+print(top_publishers)
+
+print(" ")
+sales_values = top_publishers.values
+indicator_matrix = (sales_values[:, None] > sales_values[None, :]).astype(int)
+print(indicator_matrix)
